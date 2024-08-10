3e55de6816bcca2f6d7df7b45fccd376b079ef7e
Katja <48013494+krairy@users.noreply.github.com>
2024-08-10 20:40:07 +0200

    Initial commit

---
+++ Nair/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This command will close all active conky
+killall conky
+sleep 2s
+		
+# Only the config listed below will be avtivated
+# if you want to combine with another theme, write the command here
+conky -c $HOME/.config/conky/Nair/NairM.conf &> /dev/null &
+conky -c $HOME/.config/conky/Nair/NairL.conf &> /dev/null &
+conky -c $HOME/.config/conky/Nair/NairR.conf &> /dev/null &
+
+exit
