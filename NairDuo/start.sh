4fcdec2439f9b0809fa9a0ef94a68282543f4824
Katja <48013494+krairy@users.noreply.github.com>
2024-09-23 10:38:20 +0200

    Nair without the middle panel

---
+++ NairDuo/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This command will close all active conky
+killall conky
+sleep 2s
+		
+# Only the config listed below will be avtivated
+# if you want to combine with another theme, write the command here
+conky -c $HOME/.config/conky/NairDuo/NairL.conf &> /dev/null &
+conky -c $HOME/.config/conky/NairDuo/NairR.conf &> /dev/null &
+
+exit
